6b93c6feaf20e0d98f0afe73e476cd66c22047c5
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-07-14 14:51:28 -0400

    fix typing

--- scalpyl/loaders/base.py
+++ scalpyl/loaders/base.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import IO
+from typing import IO, Optional
 from types import CodeType
 from platform import python_version_tuple
 
@@ -7,12 +7,12 @@ from platform import python_version_tuple
 class Code:
     code: CodeType
     version_tuple: tuple = python_version_tuple()
-    filename: str = None
-    timestamp: int = None
+    filename: Optional[str] = None
+    timestamp: Optional[int] = None
     is_pypy: bool = False
 
     def __init__(self, code: CodeType, version_tuple: tuple = python_version_tuple(),
-                 filename: str = None, timestamp: int = None, is_pypy: bool = False):
+                 filename: Optional[str] = None, timestamp: Optional[int] = None, is_pypy: bool = False):
         self.code = code
         self.version_tuple = version_tuple
         self.filename = filename
@@ -22,7 +22,7 @@ class Code:
 
 class Loader:
     accepted_file_types: list[str]
-    accepted_versions: list[str] = None
+    accepted_versions: Optional[list[str]] = None
 
 
     @staticmethod
