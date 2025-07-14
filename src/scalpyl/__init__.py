91db4ab192c0d807b93ac99c887fe1b0056331f8
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-07-14 16:20:20 -0400

    remove extractor from init

--- src/scalpyl/__init__.py
+++ src/scalpyl/__init__.py
@@ -8,7 +8,6 @@ from scalpyl.loaders.marshal_loader import MarshalLoader
 from scalpyl.loaders.xdis_loader import XdisLoader
 from scalpyl.loaders.source_loader import SourceLoader
 
-from scalpyl.unpackers.pyinstaller import extract_pyinstaller
 
 
 __all__ = [
@@ -16,5 +15,4 @@ __all__ = [
     "CodeBlock", "CodeFlags", "Instruction",
     "DisDisassembler", "XdisDisassembler",
     "MarshalLoader", "XdisLoader", "SourceLoader",
-    "extract_pyinstaller"
 ]
