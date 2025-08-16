85510e05c03ca20c69f91988fd65aff385e6407f
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-08-16 15:49:16 -0400

    adjust tooling

--- flake.nix
+++ flake.nix
@@ -1,130 +1,83 @@
 {
   inputs = {
-    nixpkgs.url = "flake:nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/e6f23dc08d3624daab7094b701aa3954923c6bbb";  # python 3.12.10
     flake-utils.url = "github:numtide/flake-utils";
+    nur = {
+      url = "github:hexadecimalDinosaur/nur";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = (import nixpkgs {
-          inherit system;
-          config = {
-            permittedInsecurePackages = [
-              "qtwebkit-5.212.0-alpha4"
-            ];
-          };
-        });
-        python_ver = pkgs.python312;
-        python = python_ver.override {
-          packageOverrides = pyfinal: pyprev: rec {
-            xdis = pyprev.xdis.overrideAttrs (final: old: {
-              patches = [
-                ./nixpkgs/xdis.patch
-              ];
-            });
-            decompyle3 = pyfinal.callPackage ./nixpkgs/decompyle3.nix {};
-            x-python = pyfinal.callPackage ./nixpkgs/x-python.nix {};
-            xasm = pyfinal.callPackage ./nixpkgs/xasm.nix { inherit x-python; };
-            pyinstxtractor-ng = pyfinal.callPackage ./nixpkgs/pyinstxtractor-ng.nix {};
-          };
-        };
-        python-env = (python.withPackages (p: with p; [
-          requests
-          xdis
-          uncompyle6
-          # pyqt6
-          # pyside6
-          pywebview
-          qtpy
-          flask
-          decompyle3
-          x-python
-          xasm
-          pyinstxtractor-ng
+  outputs = { nixpkgs, flake-utils, nur, ... }:
+  {
+    nixConfig = {
+      extra-substituters = [
+        "https://hexadecimaldinosaur.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "hexadecimaldinosaur.cachix.org-1:793qwEYh8UCmiTHMSFkz1/mu8mRDC7VCr7xI7e8M8s8="
+      ];
+    };
+  } //
+  flake-utils.lib.eachDefaultSystem (
+    system:
+    let
+      pkgs = (import nixpkgs {
+        inherit system;
+        overlays = [ nur.overlays.python ];
+      });
+      python-env = (pkgs.python312.withPackages (p: with p; [
+        requests
+        xdis
+        uncompyle6
+        decompyle3
+        x-python
+        xasm
+        pyinstxtractor-ng
+        dearpygui
 
-          jupyter
-          ipython
-          jupyter
-          rich
+        jupyter
+        ipython
+        jupyter
+        rich
 
-          # dev tools
-          pip
-          autopep8
-          isort
-          ptpython
-          yapf
-          pycodestyle
-          mccabe
-          pyflakes
-          mypy
-          python-lsp-server
-          pylsp-rope
-          python-lsp-ruff
-          pytest
-          pyinstaller
-          setuptools
-          build
-        ]));
-        pypy-env = (pkgs.pypy310.withPackages (p: with p; [
-          xdis
-          uncompyle6
-          self.packages.${system}.pypy310Packages.decompyle3
-          self.packages.${system}.pypy310Packages.x-python
-          self.packages.${system}.pypy310Packages.xasm
-        ]));
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            python-env
-            pypy-env
-          ];
-          buildInputs = [
-            # pkgs.webkitgtk_6_0
-            # pkgs.gtk3
-            # pkgs.qt6.full
-            # pkgs.qt6.qtwayland
-            pkgs.qt5.full
-            pkgs.qt5.qtwayland
-          ];
-          nativeBuildInputs = [
-          ];
-          shellHook = /* sh */ ''
-            export DIRENV_PYTHON=${python-env.out}
-            export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.full}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
-            export QT_PLUGINS_PATH="$QT_QPA_PLATFORM_PLUGIN_PATH";
-            export PYWEBVIEW_LOG=debug
-            export PYWEBVIEW_GUI=qt
-          '';
-        };
-        packages = {
-          pypy310Packages = rec {
-            decompyle3 = (pkgs.pypy310.pkgs.callPackage ./nixpkgs/decompyle3.nix { });
-            x-python = (pkgs.pypy310.pkgs.callPackage ./nixpkgs/x-python.nix { });
-            xasm = (pkgs.pypy310.pkgs.callPackage ./nixpkgs/xasm.nix { inherit x-python; });
-            pyinstxtractor-ng = (pkgs.pypy310.pkgs.callPackage ./nixpkgs/pyinstxtractor-ng.nix {});
-          };
-          python311Packages = rec {
-            decompyle3 = (pkgs.python311Packages.callPackage ./nixpkgs/decompyle3.nix { });
-            x-python = (pkgs.python311Packages.callPackage ./nixpkgs/x-python.nix { });
-            xasm = (pkgs.python311Packages.callPackage ./nixpkgs/xasm.nix { inherit x-python; });
-            pyinstxtractor-ng = (pkgs.python311Packages.callPackage ./nixpkgs/pyinstxtractor-ng.nix {});
-          };
-          python312Packages = rec {
-             decompyle3 = (pkgs.python312Packages.callPackage ./nixpkgs/decompyle3.nix { });
-             x-python = (pkgs.python312Packages.callPackage ./nixpkgs/x-python.nix { });
-             xasm = (pkgs.python311Packages.callPackage ./nixpkgs/xasm.nix { inherit x-python; });
-             pyinstxtractor-ng = (pkgs.python311Packages.callPackage ./nixpkgs/pyinstxtractor-ng.nix {});
-          };
-          python313Packages = rec {
-             decompyle3 = (pkgs.python313Packages.callPackage ./nixpkgs/decompyle3.nix { });
-             x-python = (pkgs.python313Packages.callPackage ./nixpkgs/x-python.nix { });
-             xasm = (pkgs.python311Packages.callPackage ./nixpkgs/xasm.nix { inherit x-python; });
-             pyinstxtractor-ng = (pkgs.python311Packages.callPackage ./nixpkgs/pyinstxtractor-ng.nix {});
-          };
-        };
-      }
-    );
+        # dev tools
+        pip
+        autopep8
+        flake8
+        isort
+        ptpython
+        yapf
+        pycodestyle
+        mccabe
+        pyflakes
+        mypy
+        python-lsp-server
+        pylsp-rope
+        python-lsp-ruff
+        pytest
+        pytest-mypy
+        pytest-flake8
+        pyinstaller
+        setuptools
+        build
+        (toPythonModule pkgs.memray)
+      ]));
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          python-env
+          pkgs.pypy310
+        ];
+        buildInputs = [
+        ];
+        nativeBuildInputs = [
+        ];
+        shellHook = /* sh */ ''
+          export DIRENV_PYTHON=${python-env.out}
+        '';
+      };
+    }
+  );
 }
